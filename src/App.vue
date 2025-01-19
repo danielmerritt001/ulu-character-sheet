39b0a29f4b4c7a11846cff5da9f101f1e367f0de
Merritt Informatics <daniel.merritt@informatics-studio.com>
2025-01-19 15:56:18 -0500

    Mount ulu to project

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <h1 class="h1">Hello World</h1>
+    <div>This is the first commit after adapting the ulu</div>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
+import './scss/_styles.scss'
 import App from './App.vue'
 
 createApp(App).mount('#app')
